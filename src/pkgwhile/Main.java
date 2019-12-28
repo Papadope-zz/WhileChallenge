128bc2566dbe5588b79481dc482657ff6027e63a
Chris Papadopoulos <xristos8170@gmail.com>
2019-12-28 13:48:06 +0200

    Add files via upload

---
+++ src/pkgwhile/Main.java
@@ -0,0 +1,37 @@
+package pkgwhile;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenNumbersFound = 0;
+        while (number <= finishNumber) {
+            number++;
+
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number " + number);
+            evenNumbersFound++;
+            if (evenNumbersFound >= 5) {
+                break;
+            }
+            
+        }
+
+        System.out.println("Total even numbers found = " + evenNumbersFound);
+
+    }
+
+    public static boolean isEvenNumber(int number) {
+        int evens = 0;
+        if ((number % 2) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
